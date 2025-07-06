57d09c6bbe10900375a19be6f6d35a622a971c28
apet2929 <realandrewpetersen@gmail.com>
2025-07-05 19:26:52 -0500

    2 toys per box

--- market.gd
+++ market.gd
@@ -16,20 +16,30 @@ var t = 0
 
 const SCROLL_RATE = 0.5 # updates per second
 
+func get_market_price():
+	return current_prices
+
 func _ready() -> void:
-	#for header in headers:
-		#data.append(PriceData.new(10, 0.3, 0.5, 10))
-	pass
+	for header in headers:
+		current_prices.append(0.0)
+		last_prices.append(0.0)
+		next_prices.append(0.0)
+	await get_tree().create_timer(SCROLL_RATE).timeout
+	
+func update_prices():
+	last_prices.clear()
+	last_prices.append_array(current_prices)
+	current_prices = next_prices
+	for i in range(0, len(next_prices)):
+		next_prices[i] = randf() - 0.5
 	
 func _draw():
-	pass
-	
-	#var bars = $Bars.get_children()
-	#for i in range(len(bars)):
-		#var bar: ColorRect = bars[i]
-		#var left = 10
-		#var right = bar.get_parent_area_size().x
-		#bar.custom_minimum_size.x = lerp(left, right, current_prices[i])
+	var bars = $Bars.get_children()
+	for i in range(len(bars)):
+		var bar: ColorRect = bars[i]
+		var left = 10
+		var right = bar.get_parent_area_size().x
+		bar.custom_minimum_size.x = lerp(float(left), float(right), float(current_prices[i]))
 	
 func _process(delta) -> void:
 	pass
